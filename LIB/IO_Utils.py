5db09ce144423c152e5f60ccc5353f8f52cf03e1
Spaceific-Arch <praktisch@email.cz>
2020-12-08 23:01:17 +0100

    first changes

---
+++ LIB/IO_Utils.py
@@ -0,0 +1,12 @@
+
+import os
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path) 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path) 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
